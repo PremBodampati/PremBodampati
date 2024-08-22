f012cf2f4ff26b2ec142f63740efb4ec70618ff4
PremBodampati <bodampatipremchand9@gmail.com>
2024-08-22 23:23:15 +0530

    my first file

---
+++ strings.py
@@ -0,0 +1,33 @@
+# firstname = "prem"
+# secondname = "chand bodampati"
+# fullname = firstname + " " + secondname + "."
+# print(fullname)
+# fullname = fullname.title()
+# print(fullname)
+#
+# emailid = "  premchand@gmail.com     "
+# emailid = len(emailid)
+#
+# print(emailid)
+#
+# emailid = "premchand@gmail.com"
+# emailid = emailid.strip()
+# emailid = len(emailid)
+# print(emailid)
+
+# couponcode = ("abc1234A")
+# couponcode = couponcode.upper()
+# print(couponcode)
+#
+# anothercoupon = "ABCabc1234abc"
+# anothercoupon = anothercoupon.lower()
+# print(anothercoupon)
+
+# name = "prem chand"
+# x = name + " yes welcome"
+# #name + "yes come in"
+# print(x)
+
+# letter = "prem chand!-"
+# letter = letter * 10
+# print(letter)
